2e2326cdab2fd71ea43d0a3044bcd99800203812
ronanhall <68893275+ronanhall@users.noreply.github.com>
2022-04-28 17:14:35 +0800

    Add files via upload
    tracks the current speed of the player in km/h

---
+++ speed_Tracker.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class speed_Tracker : MonoBehaviour
+{
+    public Rigidbody target;
+    //reference to the players rigidbody
+    
+    public TextMeshProUGUI speedCounter;
+    //the text that counts the speed of the player
+
+    private float speed = 0.0f;
+    //the speed of the player
+
+    void Update()
+    {
+        speed = target.velocity.magnitude * 3.6f;
+        //setting the speed to the velocity of the players rigidbody multiplied by 3.6 to convert it into kilometers
+
+        if (speedCounter != null)
+        {
+            speedCounter.text = ((int)speed) + "KM/H";
+        }
+        //making the spped counter equal the speed of the player plus "KM/H"
+    }
+}
