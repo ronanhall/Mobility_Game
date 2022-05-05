b4f9048f77e468bf4ea8df0c086dd4b06c9ed19c
ronanhall <68893275+ronanhall@users.noreply.github.com>
2022-05-05 16:36:56 +0800

    Update audio_Manager.cs
    adding a stopping sound method

--- audio_Manager.cs
+++ audio_Manager.cs
@@ -9,6 +9,8 @@ public class audio_Manager : MonoBehaviour
 
     public static audio_Manager instance;
 
+    public float level;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -32,22 +34,33 @@ public class audio_Manager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
-        //
+        //controls for the audio source, the name of the source, the volume and pitch, and whether it loops
     }
 
     private void Start()
     {
-        Play("Music");
+        Play("Menu Music");
     }
 
-    public void Play (string name)
+    public void Play(string name)
     {
-        sound s  = Array.Find(sounds, sound => sound.name == name);
+        sound s = Array.Find(sounds, sound => sound.name == name);
         //
         if (s == null)
             return;
         //if the sound isn't there, the game isn't trying to play it
         s.source.Play();
+        //playing the sound
     }
 
+    public void StopPlaying(string name)
+    {
+        sound s = Array.Find(sounds, sound => sound.name == name);
+        //
+        if (s == null)
+            return;
+        //if the sound isn't there, the game isn't trying to play it
+        s.source.Stop();
+        //stopping the sound
+    }
 }
