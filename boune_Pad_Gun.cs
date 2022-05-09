9fda4eabe55f6b4aab810bc1e525f2be386fe80b
ronanhall <68893275+ronanhall@users.noreply.github.com>
2022-05-09 08:58:48 +0800

    Update boune_Pad_Gun.cs

--- boune_Pad_Gun.cs
+++ boune_Pad_Gun.cs
@@ -1,34 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class boune_Pad_Gun : MonoBehaviour
+public class bounce_Pad_Gun : MonoBehaviour
 {
-    public float range = 100f; //range of the bounce pad
+    public LayerMask layerMask;
+    //the layer that the bounce pads will be instantiated on
+    
+    public float range = 100f; 
+    //range of the bounce pad
 
-    public Camera fpsCam; //reference to the camera
+    public Camera cam; 
+    //reference to the camera
 
-    public GameObject bouncePad; //reference to the bouncepad
+    public GameObject bouncePad;
+    //reference to the bouncepad
+
+    public int ammoAmount;
+    //the amount of ammo the player has
+
+    public TextMeshProUGUI ammoCounter;
+    //the text that displays the ammo
 
     // Update is called once per frame
     void Update()
     {
-        //this function basically says when the left mouse button is clocked, the player will shoot
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && ammoAmount > 0)
         {
             Shoot();
+            //shooting when the player presses dow nthe "Fire1" key
         }
+
+        ammoCounter.text = ammoAmount.ToString();
     }
 
     void Shoot()
     {
-        RaycastHit hit; //stores information about what was hit with the ray
+        RaycastHit hit; 
+        //stores information about what was hit with the ray
+        //GameObject bouncePadBullet = Instantiate(bouncePad, firePoint.position, firePoint.rotation);
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, Mathf.Infinity, layerMask))
         //this is shooting out the ray at the position of the camera on screen and forward of it
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
         {
-            Debug.Log(hit.transform.tag); //displaying objects names that are hit
+            Debug.Log(hit.transform.name); 
+            //displaying objects names that are hit
 
-            Instantiate(bouncePad, hit.point, Quaternion.LookRotation(hit.normal));
+            if (hit.collider.CompareTag("Ground"))
+            {
+                Instantiate(bouncePad, hit.point, Quaternion.LookRotation(hit.normal));
+                //if the raycast hits an object with the tag "ground", the bounce pad will instantiate
+                ammoAmount -= 1;
+                //subtracting one ammo amount whenever the player shoots
+            }
         }
+
+        
     }
 }
