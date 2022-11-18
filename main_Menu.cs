d8fc6f1b3694ae613dc2c166ae25332c3508a9e3
ronanhall <68893275+ronanhall@users.noreply.github.com>
2022-11-18 11:00:54 +0800

    finalising the main menu

--- main_Menu.cs
+++ main_Menu.cs
@@ -14,9 +14,9 @@ public class main_Menu : MonoBehaviour
         //locking cursur to the middle of the screen
         Cursor.visible = false;
         //making the cursur invisible
-        FindObjectOfType<audio_Manager>().Play("Tutorial Music");
+        FindObjectOfType<music_Manager>().Play("Tutorial Music");
         //finding the auido manager, and playing the level music
-        FindObjectOfType<audio_Manager>().StopPlaying("Menu Music");
+        FindObjectOfType<music_Manager>().StopPlaying("Menu Music");
         //finding the audio manager, and stopping the menu music
     }
 
@@ -24,15 +24,16 @@ public class main_Menu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("quitting");
+        //quiting the game
     }
 
     public void Tutorial()
     {
         SceneManager.LoadScene(1);
         //loading level 1
-        FindObjectOfType<audio_Manager>().Play("Tutorial Music");
+        FindObjectOfType<music_Manager>().Play("Tutorial Music");
         //finding the auido manager, and playing the tutorial music
-        FindObjectOfType<audio_Manager>().StopPlaying("Menu Music");
+        FindObjectOfType<music_Manager>().StopPlaying("Menu Music");
         //finding the audio manager, and stopping the menu music
     }
 
@@ -40,9 +41,9 @@ public class main_Menu : MonoBehaviour
     {
         SceneManager.LoadScene(2);
         //lodaing level 1
-        FindObjectOfType<audio_Manager>().StopPlaying("Menu Music");
+        FindObjectOfType<music_Manager>().StopPlaying("Menu Music");
         //finding the audio manager, and stopping the menu music
-        FindObjectOfType<audio_Manager>().Play("Level 1 Music");
+        FindObjectOfType<music_Manager>().Play("Level 1 Music");
         //finding the auido manager, and playing the level 1 music
     }
 
@@ -50,17 +51,19 @@ public class main_Menu : MonoBehaviour
     {
         SceneManager.LoadScene(3);
         //lodaing level 2
-        FindObjectOfType<audio_Manager>().StopPlaying("Menu Music");
+        FindObjectOfType<music_Manager>().StopPlaying("Menu Music");
         //finding the audio manager, and stopping the menu music
+        FindObjectOfType<music_Manager>().Play("Level 2 Music");
+        //finding the auido manager, and playing the level 1 music
     }
 
     public void LoadLevel3()
     {
         SceneManager.LoadScene(4);
         //lodaing level 3
-        FindObjectOfType<audio_Manager>().StopPlaying("Menu Music");
+        FindObjectOfType<music_Manager>().StopPlaying("Menu Music");
         //finding the audio manager, and stopping the menu music
-        FindObjectOfType<audio_Manager>().Play("Level 3 Music");
+        FindObjectOfType<music_Manager>().Play("Level 3 Music");
         //finding the auido manager, and playing the level 3 music
     }
 
@@ -68,9 +71,9 @@ public class main_Menu : MonoBehaviour
     {
         SceneManager.LoadScene(5);
         //lodaing level 4
-        FindObjectOfType<audio_Manager>().StopPlaying("Menu Music");
+        FindObjectOfType<music_Manager>().StopPlaying("Menu Music");
         //finding the audio manager, and stopping the menu music
-        FindObjectOfType<audio_Manager>().Play("Level 4 Music");
+        FindObjectOfType<music_Manager>().Play("Level 4 Music");
         //finding the auido manager, and playing the level 4 music
     }
 
@@ -78,7 +81,7 @@ public class main_Menu : MonoBehaviour
     {
         SceneManager.LoadScene(6);
         //lodaing level 5
-        FindObjectOfType<audio_Manager>().StopPlaying("Menu Music");
+        FindObjectOfType<music_Manager>().StopPlaying("Menu Music");
         //finding the audio manager, and stopping the menu music
     }
 
