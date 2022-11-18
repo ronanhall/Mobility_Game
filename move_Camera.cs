9153e32e413d3759847a625066a57c3ee4a6512c
ronanhall <68893275+ronanhall@users.noreply.github.com>
2022-11-18 10:23:20 +0800

    finalising the move camera

--- move_Camera.cs
+++ move_Camera.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class move_Camera : MonoBehaviour
 {
     [SerializeField] Transform cameraPosition;
+    //the transform of the camera's position
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = cameraPosition.position; //setting the position to the camera holders position
+        transform.position = cameraPosition.position; 
+        //setting the position to the camera holders position
     }
 }
