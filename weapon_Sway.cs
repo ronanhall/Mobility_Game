6e65eec6905ac9ce7d12edf327c09127bb9e420e
ronanhall <68893275+ronanhall@users.noreply.github.com>
2022-11-18 10:58:46 +0800

    finalising the weapon sway

--- weapon_Sway.cs
+++ weapon_Sway.cs
@@ -11,16 +11,6 @@ public class weapon_Sway : MonoBehaviour
     //the sway multiplier
 
 
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
